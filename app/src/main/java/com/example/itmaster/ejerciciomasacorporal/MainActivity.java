658084fecc58794d3dcf460132abdf405c573b87
modicristian <cristianmodi@gmail.com>
2018-10-04 21:10:08 -0300

    Inventando

--- app/src/main/java/com/example/itmaster/ejerciciomasacorporal/MainActivity.java
+++ app/src/main/java/com/example/itmaster/ejerciciomasacorporal/MainActivity.java
@@ -2,12 +2,30 @@ package com.example.itmaster.ejerciciomasacorporal;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private android.support.v7.widget.Toolbar toolbar;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getActionBar().setDisplayShowTitleEnabled(true);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setDisplayUseLogoEnabled(false);
+        getActionBar().setTitle("CALCULAR TU MASA CORPORAL");
     }
+
+
+
+
 }
